9769fa33b7e9de822a3825bc56196b72e127278c
Bair Suimaliev <71211801+Benefactor131@users.noreply.github.com>
2022-03-15 18:31:16 -0400

    Update main.js

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -11,9 +11,13 @@ const Main = ({ children, router}) => {
     <Box as="main" pb={8}>
         <Head>
             <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <meta name="description" content="Bair's homepage" />
             <meta name="author" content="Bair Suimaliev" />
-            <meta name="author" content="bsuimaliev" />
+            <meta property='og:title' content='Bsuimaliev'/>
+            <meta name="image" property='og:image' content='https://i.ibb.co/BKY6nHB/bsuimaliev.png'/>
+            <meta name="description" property='og:description' content='My portfolio website, featuring my projects done by myself and during my studies at Seneca College Computer Programming course.'/>
+            <meta property='og:url' content='https://bsuimaliev.herokuapp.com/'/>
+            <meta property='og:image:width' content='1200' />
+            <meta property='og:image:height' content='627' />
             <link rel="apple-touch-icon" href="apple-touch-icon.png" />
             <link rel="shortcut icon" href="/favicon.ico" type="image/png" />
             <meta property="og:site_name" content="Bair Suimaliev's Homepage" />
