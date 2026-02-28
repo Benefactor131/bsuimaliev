eb0115dc1c63931e4c81275cdab7999042b287ce
Bair Suimaliev <71211801+Benefactor131@users.noreply.github.com>
2026-02-28 12:32:56 +0600

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -59,7 +59,6 @@ const Navbar = props => {
             >
                 <Flex align="center" mr={5}>
                     <Heading as="h1" size="lg" letterSpacing={'tighter'}>
-                        <Logo />
                     </Heading>
                 </Flex>
 
